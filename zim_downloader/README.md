04bda8c74def6889832af76476f75ececc17ceab
Peter Aufner <peter@code-alongsi.de>
2023-10-23 21:54:05 +0200

    Add downloader script for zim files

---
+++ zim_downloader/README.md
@@ -0,0 +1,6 @@
+# zim_downloader
+
+This script downloads the latest version of zim files from the kiwix.org FTP server.
+Provide the download path as an argument.
+Set the files to download inside the "zims" dictionary of the script. 
+
---
+++ zim_downloader/zim_downloader.py
@@ -0,0 +1,53 @@
+from fs import open_fs
+from datetime import datetime
+import os, sys
+
+zim_files = open_fs('ftp://download.kiwix.org/zim/')
+path = sys.argv[1]
+
+current_dir_list = (file for file in os.listdir(path) 
+         if os.path.isfile(os.path.join(path, file)))
+print(current_dir_list)
+
+date_format = '%Y-%m'
+
+zims ={'wikipedia':'_en_all_nopic_', 'wiktionary':'_en_all_maxi_', 'wikiquote':'_en_all_maxi_'}
+
+for zim in zims.keys():
+    base_name = zim + zims[zim]
+    local_zim_version = datetime.strptime('1970-01', date_format)
+    local_zim_version_name = base_name + '1970-01.zim'
+
+    for file in current_dir_list:
+        if base_name in file:
+            local_zim_version = datetime.strptime(file.split(base_name)[1].split('.zim')[0], date_format)
+            local_zim_version_name = file
+            break
+
+    print("Local zim version: " + str(local_zim_version))
+    print("Local zim name: " + local_zim_version_name)
+
+    current_zim = zim_files.opendir(zim)
+    server_zim_version = datetime.strptime('1970-01', date_format)
+    server_zim_version_name = ''
+    for zim_version in current_zim.walk.files(filter=[base_name + '*.zim']):
+        date = datetime.strptime(zim_version.split(base_name)[1].split('.zim')[0], date_format)
+        if date > local_zim_version:
+            server_zim_version = date
+            server_zim_version_name = zim_version[1:]
+    print("Server zim version: " + str(server_zim_version))
+    print("Server zim name: " + server_zim_version_name)
+
+    if server_zim_version > local_zim_version:
+        print("Newer version found!")
+        print("Downloading {}".format(server_zim_version_name))
+        with open(os.path.join(path, server_zim_version_name), 'wb') as write_file:
+            zim_files.download(zim + '/' + server_zim_version_name, write_file)
+        print("Download finished")
+        print("Deleting old version: {}".format(local_zim_version_name))
+        try:
+            os.remove(os.path.join(path, local_zim_version_name))
+        except FileNotFoundError:
+            pass
+    else:
+        print("Already at the newest version!")
